8bd390f45a31b33bd91b23ff1299b5f9fa19449f
Dangergrow <Dangergrow@gmail.com>
2023-08-06 10:32:08 +0300

    Added

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticketPrice = 13676;
+        int x = 20;     //количество рублей для одной бонусной мили
+        int BonusMile = ticketPrice / x;
+        System.out.println(BonusMile);
+    }
+}
